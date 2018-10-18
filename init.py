88fbf2376a57ff6611c2fceba6697f5e2642d1a9
= <aaron.chambers81@gmail.com>
2018-10-18 17:14:08 +0000

    added resume site, added theme

--- init.py
+++ init.py
@@ -4,6 +4,7 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
---
+++ static/scripts/resume.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+
+	function shuffle(array) {
+	  	var currentIndex = array.length, temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+	  	return array;
+	}
+
+	// propegate technologies chips
+    $.getJSON( "/static/json/tech.json", function( data ) {
+    	var items = []
+		$.each( data, function( key, val ) {
+			for(i = 0; i < val.length; i++) {
+				color = ""
+				switch(key) {
+					case "CPP":
+						color = "success"
+						break;
+					case "Java":
+						color = "primary"
+						break;
+					case "Python":
+						color = "danger"
+						break;
+					case "Web":
+						color = "warning"
+						break;
+					case "OS":
+						color = "secondary"
+						break;
+					case "Misc":
+						color = "dark"
+						break;
+				}
+				items.push( "<button type='button' style='margin-right:20px' class='btn btn-disabled btn-outline-" + color + " tech-" + key + "' disabled>" + val[i] + "</button>" );
+			}
+		});
+
+		items = shuffle(items)
+
+		while(items.length > 0) {
+			app = "<div class='row form-group'>"
+			for(i = 0; i < 6; i++) {
+				app += items.pop()
+				if(items.length <= 0) {
+					break
+				}
+			}
+			app += "</div>"
+			$(".technologies-values").append(app)
+		}
+	});
+
+	$(".technology-key").hover(
+		function() {
+    		tech_class = "tech-" + $(this).attr("data-tech")
+    		tech_color = "" + $(this).attr("data-techCol")
+    		$("."+tech_class).removeClass("btn-outline-" + tech_color)
+    		$("."+tech_class).addClass("btn-" + tech_color)
+  		}, function() {
+    		tech_class = "tech-" + $(this).attr("data-tech")
+    		tech_color = "" + $(this).attr("data-techCol")
+    		$("."+tech_class).removeClass("btn-" + tech_color)
+    		$("."+tech_class).addClass("btn-outline-" + tech_color)
+  	})
+});
