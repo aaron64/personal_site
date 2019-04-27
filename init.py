8135744ce452457f31d839bb8b2fb80cf0074314
Aaron Chambers <aaron.chambers81@gmail.com>
2019-04-27 15:09:39 -0400

    removed lights

--- init.py
+++ init.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
-from static.python.magichome import MagicHomeApi
+#from flask_flatpages import FlatPages
 
 app = Flask(__name__)
+
+def prerender_jinja(text):
+    prerendered_body = render_template_string(Markup(text))
+    return pygmented_markdown(prerendered_body)
+app.config['FLATPAGES_HTML_RENDERER'] = prerender_jinja
 
 def render_header():
     return render_template("header.html")
@@ -30,30 +34,3 @@ def projects(p = None):
     site += render_template("projects.html", project=p)
     site += render_template("footer.html")
     return site
-
-@app.route('/tools')
-def tools():
-    site = render_template("header.html")
-    site += render_template("tools.html")
-    site += render_template("footer.html")
-    return site
-
-@app.route('/light_on')
-def light_on():
-    controller = MagicHomeApi('10.10.123.3', 0)
-    controller.turn_on()
-    controller.update_device(255, 160, 45, 50)
-    return (''), 204
-
-@app.route('/light_off')
-def light_off():
-    controller = MagicHomeApi('10.10.123.3', 0)
-    controller.turn_off()
-    return (''), 204
-
-@app.route('/light_color/<r>/<g>/<b>')
-def light_color(r = None, g = None, b = None):
-    controller = MagicHomeApi('10.10.123.3', 0)
-    controller.turn_on()
-    controller.update_device(int(r), int(g), int(b), 50)
-    return (''), 204
